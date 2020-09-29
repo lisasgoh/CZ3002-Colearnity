a4d200091f324447b4e9678428e6bed8646ef4ae
jiajin9 <jiajing0205@gmail.com>
2020-09-30 02:20:12 +0800

    Revert "Revert "backend skeleton code""
    This reverts commit 165478a46c47107b200a9f1a9bb5c0d9ba678593.

---
+++ backend/app.js
@@ -0,0 +1,60 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// json parser
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+// insert POST/GET functions
+
+// test functions
+
+app.get('/account/UUID', function (req, res) {
+  res.send('Here is your data')
+})
+
+app.post('/account/create', function (request, response) {
+  console.log(request.body);
+  // validate request.body and create account or send error msg
+  response.send('got it')
+})
+
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
