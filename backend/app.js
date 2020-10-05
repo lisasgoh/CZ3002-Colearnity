5ddc29002d145395d188d40b24d3a6cb08fd7013
lisasgoh <42923450+lisasgoh@users.noreply.github.com>
2020-10-06 01:13:21 +0800

    Add mongodb connection

--- backend/app.js
+++ backend/app.js
@@ -1,62 +1,80 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var commentRouter = require("./routes/comment");
+var postRouter = require("./routes/post");
+
+const mongoose = require("mongoose");
 
 var app = express();
 
 // json parser
-var bodyParser = require('body-parser');
+var bodyParser = require("body-parser");
 app.use(bodyParser.json());
+
+// connect to mongodb
+const url = `mongodb+srv://colearnity:${password}@cluster0.4j8bx.mongodb.net/<dbname>?retryWrites=true&w=majority`;
+
+mongoose
+  .connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then((result) => {
+    console.log("connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log("error connecting to MongoDB:", error.message);
+  });
 
 // insert POST/GET functions
 
 // test functions
 
-app.get('/account/UUID', function (req, res) {
-  res.send('Here is your data')
-})
+app.get("/account/UUID", function (req, res) {
+  res.send("Here is your data");
+});
 
-app.post('/account/create', function (request, response) {
+app.post("/account/create", function (request, response) {
   console.log(request.body);
   // validate request.body and create account or send error msg
-  response.send('got it')
-})
-
-
-
+  response.send("got it");
+});
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
