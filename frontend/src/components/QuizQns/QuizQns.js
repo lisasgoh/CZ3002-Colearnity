d65af4c180661df37792911a9036a77833508a34
bryna <brynagoh@gmail.com>
2020-10-09 01:17:13 +0800

    adds reviewquizpage and alters quizqns props

--- frontend/src/components/QuizQns/QuizQns.js
+++ frontend/src/components/QuizQns/QuizQns.js
@@ -1,44 +1,60 @@
-import React from 'react'
+import React from "react";
 import {
-    Radio,
-    RadioGroup,
-    FormControlLabel,
-    FormControl
-} from '@material-ui/core';
-import './QuizQns.css'
+  Radio,
+  RadioGroup,
+  FormControlLabel,
+  FormControl,
+} from "@material-ui/core";
+import "./QuizQns.css";
 
 export default function QuizQns(props) {
-    // state = {
-    //     userAns: null,
-    //     curQns: 0,
-    //     options: []
-    // }
+  // state = {
+  //     userAns: null,
+  //     curQns: 0,
+  //     options: []
+  // }
 
-    const [value, setValue] = React.useState('Option 1');
+  const [value, setValue] = React.useState("Option 1");
 
-    const handleChange = (event) => {
-        setValue(event.target.value);
-    };
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
 
-    return (
-        <div className="question">
-            <div className="qnsheader">
-                <h6>Question no.</h6>
-                <h6>10 marks</h6>
-            </div>
+  return (
+    <div className="question">
+      <div className="qnsheader">
+        <h6>Question no. {props.qnNum}</h6>
+        <h6>{props.qnWeightage} marks</h6>
+      </div>
 
-            <p>Which is not a divisor of 36?</p>
+      <p>Which is not a divisor of 36?</p>
 
-            <div className="options">
-                <FormControl component="fieldset">
-                    <RadioGroup value={value} onChange={handleChange}>
-                        <FormControlLabel value="option 1" control={<Radio />} label="Option 1" />
-                        <FormControlLabel value="option 2" control={<Radio />} label="Option 2" />
-                        <FormControlLabel value="Option 3" control={<Radio />} label="Option 3" />
-                        <FormControlLabel value="Option 4" control={<Radio />} label="Option 4" />
-                    </RadioGroup>
-                </FormControl>
-            </div>
-        </div>
-    )
+      <div className="options">
+        <FormControl component="fieldset">
+          <RadioGroup value={value} onChange={handleChange}>
+            <FormControlLabel
+              value="option 1"
+              control={<Radio />}
+              label="Option 1"
+            />
+            <FormControlLabel
+              value="option 2"
+              control={<Radio />}
+              label="Option 2"
+            />
+            <FormControlLabel
+              value="Option 3"
+              control={<Radio />}
+              label="Option 3"
+            />
+            <FormControlLabel
+              value="Option 4"
+              control={<Radio />}
+              label="Option 4"
+            />
+          </RadioGroup>
+        </FormControl>
+      </div>
+    </div>
+  );
 }
---
+++ frontend/src/container/ReviewQuizPage/ReviewQuizPage.js
@@ -0,0 +1,63 @@
+import React, { Component } from "react";
+import Post from "../../components/Post/Post";
+import QuizButton from "../../components/ForumButtons/QuizButton";
+import Button from "@material-ui/core/Button";
+import QuizQns from "../../components/QuizQns/QuizQns";
+import Divider from "@material-ui/core/Divider";
+import "./ReviewQuizPage.css";
+
+class ReviewQuizPage extends Component {
+  render() {
+    return (
+      <div className="reviewquizpage">
+        <div className="leftsection">
+          <h2>Software Quality Management</h2>
+          <Divider variant="middle" />
+
+          <h3>Quizzes</h3>
+          <div className="quizzes">
+            <QuizButton
+              quizTitle="Quiz 1"
+              completed={true}
+              completionDate="11/9/2020"
+              grade="10/10"
+            />
+            <QuizButton
+              quizTitle="Quiz 2"
+              completed={true}
+              completionDate="25/9/2020"
+              grade="6/10"
+            />
+            <QuizButton
+              quizTitle="Quiz 3"
+              completed={false}
+              dueDate="25/12/2020"
+            />
+          </div>
+        </div>
+
+        <div className="rightsection">
+          <div className="topbar">
+            <h2>Quiz 2: Review</h2>
+            <h6>GRADE HERE IDK</h6>
+          </div>
+          <QuizQns qnNum="1" qnWeightage="10" />
+          <QuizQns qnNum="2" qnWeightage="10" />
+          <QuizQns qnNum="3" qnWeightage="10" />
+          <QuizQns qnNum="4" qnWeightage="10" />
+          <QuizQns qnNum="5" qnWeightage="10" />
+          <QuizQns qnNum="6" qnWeightage="10" />
+          <Button>
+            <h4>Review Concepts</h4>
+            <p>
+              retake your quiz to review your concepts! note: future attempts
+              will not be graded
+            </p>
+          </Button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ReviewQuizPage;
