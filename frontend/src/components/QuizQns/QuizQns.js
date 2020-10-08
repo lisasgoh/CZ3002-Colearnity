6d966c4a3681c63b231c671f1bf715fc9ebbb3c6
chongyoumin <56867168+chongyoumin@users.noreply.github.com>
2020-10-08 23:07:30 +0800

    added quiz qn component

---
+++ frontend/src/components/QuizQns/QuizQns.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import {
+    Radio,
+    RadioGroup,
+    FormControlLabel,
+    FormControl
+} from '@material-ui/core';
+import './QuizQns.css'
+
+export default function QuizQns(props) {
+    // state = {
+    //     userAns: null,
+    //     curQns: 0,
+    //     options: []
+    // }
+
+    const [value, setValue] = React.useState('Option 1');
+
+    const handleChange = (event) => {
+        setValue(event.target.value);
+    };
+
+    return (
+        <div className="question">
+            <div className="qnsheader">
+                <h6>Question no.</h6>
+                <h6>10 marks</h6>
+            </div>
+
+            <p>Which is not a divisor of 36?</p>
+
+            <div className="options">
+                <FormControl component="fieldset">
+                    <RadioGroup value={value} onChange={handleChange}>
+                        <FormControlLabel value="option 1" control={<Radio />} label="Option 1" />
+                        <FormControlLabel value="option 2" control={<Radio />} label="Option 2" />
+                        <FormControlLabel value="Option 3" control={<Radio />} label="Option 3" />
+                        <FormControlLabel value="Option 4" control={<Radio />} label="Option 4" />
+                    </RadioGroup>
+                </FormControl>
+            </div>
+        </div>
+    )
+}
