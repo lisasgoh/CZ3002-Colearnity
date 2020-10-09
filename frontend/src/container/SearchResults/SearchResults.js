81b6a5f49e583ca34e63e6f58ae3381f5171bc90
bryna <brynagoh@gmail.com>
2020-10-09 22:51:21 +0800

    adds searchresults page
    not yet dynamic to data

---
+++ frontend/src/container/SearchResults/SearchResults.js
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+import Post from "../../components/Post/Post";
+import ForumButton from "../../components/ForumButtons/ForumButton";
+import Filter from "../../components/Filter/Filter";
+import "./SearchResults.css";
+
+class SearchResults extends Component {
+  render() {
+    const { classes } = this.props;
+    return (
+      <div className="searchresults">
+        <div className="leftsection">
+          <h2>Related Forums</h2>
+          <div className="forums">
+            <ForumButton
+              color="papayawhip"
+              hovercolor="peachpuff"
+              forumTitle="CZ3002 ASE"
+            />
+            <ForumButton
+              color="lightcyan"
+              hovercolor="darkcyan"
+              forumTitle="CZ3001 ACOA"
+            />
+            <ForumButton
+              color="lavender"
+              hovercolor="darkslateblue"
+              forumTitle="CZ1007 Data Structures"
+            />
+          </div>
+        </div>
+
+        <div className="rightsection">
+          <div className="topbar">
+            <h2>Recent Posts for searchQuery (not sure how to dynamic)</h2>
+
+            <Filter />
+          </div>
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default SearchResults;
