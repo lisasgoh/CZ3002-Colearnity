ede82c9640b1ad5c6de371c6a3cfa7743f59c794
jiajin9 <jiajing0205@gmail.com>
2020-10-11 13:42:31 +0800

    fixed token not found when trying to log in with token

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,15 +1,26 @@
 const jwt = require("express-jwt");
 
+/*
 const getTokenFromHeaders = (req) => {
   const {
     headers: { authorization },
   } = req;
 
-  if (authorization && authorization.split(" ")[0] === "Token") {
+  if (authorization && authorization.split(" ")[0] === "token") {
+    console.log(authorization.split(" ")[1]);
     return authorization.split(" ")[1];
   }
   return null;
 };
+*/
+
+const getTokenFromHeaders = (req) => {
+
+  if (req.headers.token) 
+    return req.headers.token;
+  return null;
+};
+
 
 const auth = {
   required: jwt({
