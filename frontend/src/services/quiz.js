e7bb40f5189e4ced8bad2995d83f8620d98ce63b
lisasgoh <42923450+lisasgoh@users.noreply.github.com>
2020-10-24 23:28:23 +0800

    Add quiz services

--- frontend/src/services/quiz.js
+++ frontend/src/services/quiz.js
@@ -1,11 +1,24 @@
 import axios from "axios";
-const baseUrl = "http://localhost:3000/api/quiz"; //NOT SURE THE BASEURL
-
+const baseUrl = "http://localhost:3000/api/quiz";
 const getQuiz = (id) => {
   const request = axios.get(`${baseUrl}/${id}`);
   return request.then((response) => response.data);
 };
 
+const postQuiz = (newObject, forum_id) => {
+  const request = axios({
+    method: "post",
+    url: `${baseUrl}?forum_id=${forum_id}`,
+    data: newObject,
+  });
+  console.log(request.then((response) => response.data));
+  return request.then((response) => response.data);
+}
+
+const deleteObj = (id) => {
+  return axios.delete(`${baseUrl}/${id}`);
+};
+
 export default {
-  getQuiz,
+  getQuiz, postQuiz,  deleteObj
 };
