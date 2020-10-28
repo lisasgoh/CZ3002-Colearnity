b20dd94ca243cd7aa800a0cecd05eac6ebb8c75e
xinhuikang <xinhuikangkxh@gmail.com>
2020-10-28 22:09:38 +0800

    for searching

--- backend/routes/search.js
+++ backend/routes/search.js
@@ -1,15 +1,31 @@
 const express = require('express');
 const Post = require('../models/Post');
+const Forum = require('../models/Forum');
 
 const searchRouter = express.Router();
 
-// get post(s) based on search keywords post info - /api/search?searchString=(words)
+// get post(s) based on search keywords post info - /api/search?postKeyWord=(words)
 searchRouter.get('/', (req, res) => {
-  // console.log("search for keyword(s) received: " +req.query.searchString);
-  const str = new RegExp(`.*${req.query.searchString}.*`, 'i');
+  // console.log("search for keyword(s) received: " +req.query.postKeyword);
+  const str = new RegExp(`.*${req.query.postKeyword}.*`, 'i');
   Post.find({
     $or: [
       { title: str },
+      { description: str },
+    ],
+  }, (err, post) => {
+    if (err) {
+      res.send(err);
+    } else { res.json(post); }
+  });
+});
+
+// get post(s) based on search keywords post info - /api/search?forumKeyword=(words)
+searchRouter.get('/', (req, res) => {
+  const str = new RegExp(`.*${req.query.forumKeyword}.*`, 'i');
+  Forum.find({
+    $or: [
+      { name: str },
       { description: str },
     ],
   }, (err, post) => {
--- frontend/src/services/search.js
+++ frontend/src/services/search.js
@@ -2,11 +2,16 @@ import axios from "axios";
 const baseUrl = "http://localhost:3000/api/search"; 
 
 const searchPost = (keyword) => {
-  const request = axios.get(`${baseUrl}?seachString=${keyword}`);
+  const request = axios.get(`${baseUrl}?postKeyword=${keyword}`);
   return request.then((response) => response.data);
+};
+
+const searchForum = (keyword) => {
+    const request = axios.get(`${baseUrl}?forumKeyword=${keyword}`);
+    return request.then((response) => response.data);
 };
 
 
 export default {
-  searchPost,
+  searchPost, searchForum,
 };
