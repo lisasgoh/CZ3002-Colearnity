328e7eadde4c66fcfe1c556a0fdd10d8df534479
lisasgoh <42923450+lisasgoh@users.noreply.github.com>
2020-11-01 01:12:49 +0800

    fix forum bug

--- frontend/src/services/forum.js
+++ frontend/src/services/forum.js
@@ -2,7 +2,14 @@ import axios from "axios";
 const baseUrl = "http://localhost:3000/api/forum";
 
 const getForum = (id) => {
-  const request = axios.get(`${baseUrl}/${id}`);
+  const request = axios({
+    method: 'get',
+    url: `${baseUrl}/${id}`,
+    headers: {
+      "token": localStorage.getItem("token") 
+    },
+    withCredentials: true,
+  });
   return request.then((response) => response.data);
 };
 
