73fd3bd02c3f9a0a6359fa9a0a76ecb01d594fb9
xinhuikang <xinhuikangkxh@gmail.com>
2020-11-01 10:46:28 +0800

    Update forum.js

--- backend/routes/forum.js
+++ backend/routes/forum.js
@@ -24,7 +24,8 @@ forumRouter.post('/', (req, res) => {
     is_sub: req.body.is_sub,
     _parentforum: req.query.forum_id,
   });
-  if (req.body.is_sub === 'true') { // add parent forum id and subforums list
+  // console.log(typeof req.body.is_sub);
+  if (req.body.is_sub === true) { // add parent forum id and subforums list
     console.log('IS SUBFORUM');
     forum.save().then((forum) => {
       console.log(forum);
