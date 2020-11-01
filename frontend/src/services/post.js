e2172394d6314263607d1b6760b849689fa13bac
lisasgoh <42923450+lisasgoh@users.noreply.github.com>
2020-11-01 21:52:49 +0800

    Add credentials for get post

--- frontend/src/services/post.js
+++ frontend/src/services/post.js
@@ -2,17 +2,17 @@ import axios from "axios";
 const baseUrl = "http://localhost:3000/api/posts";
 
 const getIndivPost = (id) => {
-  const request = axios.get(`${baseUrl}/${id}`);
-  return request.then((response) => response.data);
-};
-const getForumPosts = (forum_id) => {
-  const request = axios.get(baseUrl, {
-    params: {
-      forum_id: forum_id,
+  const request = axios({
+    method: "get",
+    url: `${baseUrl}/${id}`,
+    headers: {
+      "token": localStorage.getItem("token")  
     },
+    withCredentials: true,
   });
   return request.then((response) => response.data);
 };
+
 const create = (newObject, forum_id) => {
   // const auth_token =
    //  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Imxpc2EzNjQwNUBnbWFpbC5jb20iLCJpZCI6IjVmN2Y1MjVkNTZiOTgzNWIyNDVlOGFhZiIsImV4cCI6MTYwNzYxNzQ4NywiaWF0IjoxNjAyNDMzNDg3fQ.xniUrdSGgfPDBXX6AJ-NmRKWkQHk5sPA4HZbTZ16C0A";
@@ -38,4 +38,4 @@ const deleteObj = (id) => {
   return axios.delete(`${baseUrl}/${id}`);
 };
 
-export default { getIndivPost, getForumPosts, create, update, deleteObj };
+export default { getIndivPost, create, update, deleteObj };
