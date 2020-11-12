b3460bd34bbcf5e54f13a228bfd112fe175bd798
bryna <brynagoh@gmail.com>
2020-11-12 23:49:03 +0800

    removes logout from navbar

--- frontend/src/components/Navbar/MenuItems.js
+++ frontend/src/components/Navbar/MenuItems.js
@@ -1,17 +1,17 @@
 export const MenuItems = [
-    {
-        title: 'Home',
-        url: '/homepage',
-        cName: 'nav-links'
-    },
-    {
-        title: 'My Account',
-        url: '/profilepage',
-        cName: 'nav-links'
-    },
-    {
-        title: 'Log Out',
-        url: '#',
-        cName: 'nav-links-mobile'
-    }
-]
+  {
+    title: "Home",
+    url: "/homepage",
+    cName: "nav-links",
+  },
+  {
+    title: "My Account",
+    url: "/profilepage",
+    cName: "nav-links",
+  },
+  //     {
+  //         title: 'Log Out',
+  //         url: '#',
+  //         cName: 'nav-links-mobile'
+  //     }
+];
